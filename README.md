32eb4e1571c9a3cf9d41d423593a6872b005279a
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2021-11-26 19:05:51 +0000

    Added Age Badge

--- README.md
+++ README.md
@@ -10,6 +10,19 @@ How to use:
 
     ![Views](https://dynamic-badges.maxalpha.repl.co/views?id=Username.PageID&style=for-the-badge&color=yellow)
 
+# Age Badge
+
+![Age](https://dynamic-badges.maxalpha.repl.co/age/12/08/2004)
+
+How to use:
+
+  ![Age](https://dynamic-badges.maxalpha.repl.co/age/12/08/2004)
+
+Parameters : 
+
+    color
+    style
+
 # Animated SVG Text
 
 ![](https://dynamic-badges.maxalpha.repl.co/animated-svg?text=I%20AM%20Jaysmito&duration=3&color=%237d403c)
--- index.js
+++ index.js
@@ -62,29 +62,29 @@ app.get('/views', (req, res) => {
 
 app.get('/animated-svg', (req, res) => {
   res.set({
-  'Content-Type': 'image/svg+xml'
-});
-var duration = 5;
-if(req.query.duration)
-  duration = req.query.duration;
-var color = "#9e4a44";
-if(req.query.color)
-  color = req.query.color;
-var text = "Dynamic Badges/SVG Animated Text";
-if(req.query.text)
-  text = req.query.text;
-var repeatCount = "indefinite";
-if(req.query.repeatCount)
-  repeatCount = req.query.repeatCount;
-var font = "Montserrat";
-if(req.query.font)
-  font = req.query.font;
-var fontSize = 26;
-if(req.query.fontSize)
-  fontSize = req.query.fontSize;
+    'Content-Type': 'image/svg+xml'
+  });
+  var duration = 5;
+  if (req.query.duration)
+    duration = req.query.duration;
+  var color = "#9e4a44";
+  if (req.query.color)
+    color = req.query.color;
+  var text = "Dynamic Badges/SVG Animated Text";
+  if (req.query.text)
+    text = req.query.text;
+  var repeatCount = "indefinite";
+  if (req.query.repeatCount)
+    repeatCount = req.query.repeatCount;
+  var font = "Montserrat";
+  if (req.query.font)
+    font = req.query.font;
+  var fontSize = 26;
+  if (req.query.fontSize)
+    fontSize = req.query.fontSize;
   res.send(
     `
-    <svg width="100%" height="100%" viewBox="30 -50 600 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+    <svg width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
  <path id="path">
 		<animate attributeName="d" from="m0,110 h0" to="m0,110 h1100" dur="${duration}s" begin="0s" repeatCount="${repeatCount}"/>
 	</path>
@@ -94,6 +94,52 @@ if(req.query.fontSize)
 </svg>
 `
   );
+});
+
+app.get('/api/age/:dd/:mm/:yyyy/:color/:style', (req, res) => {
+  var day = req.params.dd;
+  var month = req.params.mm;
+  var year = req.params.yyyy;
+  const rawMiliseconds = new Date() - new Date(`${day}/${month}/${year}`);
+  const rawSeconds = Math.floor(rawMiliseconds / 1000);
+  const rawMinutes = Math.floor(rawSeconds / 60);
+  const rawHours = Math.floor(rawMinutes / 60);
+  const rawDays = Math.floor(rawHours / 24);
+  const rawMonths = Math.floor(rawDays / 30);
+  const rawYears = Math.floor(rawDays / 365);
+
+  const seconds = rawSeconds % 60;
+  const minutes = rawMinutes % 60;
+  const hours = rawHours % 24;
+  const days = rawDays % 30;
+  const months = rawMonths % 12;
+  const years = rawYears;
+  var col = "green";
+  var st = "for-the-badge";
+  if (req.params.color)
+    col = req.params.color;
+  if (req.params.style)
+    st = req.params.style;
+
+  var output = {
+    "schemaVersion": 1,
+    "label": "Age",
+    "message": `${years} years, ${months} months, ${days} days`,
+    "color": col,
+    "style": st
+  };
+  res.json(output);
+});
+
+app.get('/age/:dd/:mm/:yyyy', (req, res) => {
+  var col = "green";
+  var st = "for-the-badge";
+  if (req.query.color)
+    col = req.query.color;
+  if (req.query.style)
+    st = req.query.style;
+  res.redirect("https://img.shields.io/endpoint?url=" + encodeURIComponent(`https://dynamic-badges.maxalpha.repl.co/api/age/${req.params.dd}/${req.params.mm}/${req.params.yyyy}/${col}/${st}`));
+  
 });
 
 app.get('/*', (req, res) => {
